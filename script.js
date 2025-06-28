1964af4507c40401618a0ac68d0a08cffe80966a
Lucas <lucthe@alunos.utfpr.edu.br>
2025-06-27 21:59:02 -0300

    #02

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-alert('Atividade cauculo de media calculo de média!');
+alert('Atividade cauculo de média!');
 
 let quntidade;
 let nota;
 let media = 0;
 quntidade = parseInt(prompt('Quantas notas você quer inserir?'));
 for (i = 0; i < quntidade; i++) {
-    nota = parseFloat(prompt('Digite a ' + (x + 1) + 'nota: '));
-    
-    console.log('Nota ' + (x + 1) + ': ' + nota);
-    
+    nota = parseFloat(prompt('Digite a ' + (i + 1) + 'nota: '));
     media += nota;
 }
 alert('Média: ' + (media / tamanho).toFixed(2));
